519f4a010f4af6afd5cc670366c5894efaed9b01
Damien Theys <damien@hyseco.be>
2023-12-05 16:09:15 +0100

    register and login response

---
+++ Sources/HysecoShowerAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,23 @@
+//
+//  File.swift
+//
+//
+//  Created by Damien Theys on 21/11/2023.
+//
+
+
+import Foundation
+
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
+}
---
+++ Sources/HysecoShowerAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//
+//
+//  Created by Damien Theys on 21/11/2023.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+}
