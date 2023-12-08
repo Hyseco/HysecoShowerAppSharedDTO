045caf568df56fc1256e35125c6205e8aed82c55
Damien Theys <damien@hyseco.be>
2023-12-08 09:27:55 +0100

    new fields in categories dtos

--- Sources/HysecoShowerAppSharedDTO/HysecoCategoryRequestDTO.swift
+++ Sources/HysecoShowerAppSharedDTO/HysecoCategoryRequestDTO.swift
@@ -11,9 +11,18 @@ public struct HysecoCategoryRequestDTO: Codable {
     
     public let title: String
     public let image: String
+    public let realisations: String
+    public let brochure: String
+    public let options: String
+    public let video: String
+
     
-    public init(title: String, image: String) {
+    public init(title: String, image: String, realisations: String, brochure: String, options: String, video: String) {
         self.title = title
         self.image = image
+        self.realisations = realisations
+        self.brochure = brochure
+        self.options = options
+        self.video = video
     }
 }
--- Sources/HysecoShowerAppSharedDTO/HysecoCategoryResponseDTO.swift
+++ Sources/HysecoShowerAppSharedDTO/HysecoCategoryResponseDTO.swift
@@ -12,10 +12,18 @@ public struct HysecoCategoryResponseDTO: Codable {
     public let id: UUID
     public let title: String
     public let image: String
-    
-    public init(id: UUID, title: String, image: String) {
+    public let realisations: String
+    public let brochure: String
+    public let options: String
+    public let video: String
+
+    public init(id: UUID, title: String, image: String, realisations: String, brochure: String, options: String, video: String) {
         self.id = id
         self.title = title
         self.image = image
+        self.realisations = realisations
+        self.brochure = brochure
+        self.options = options
+        self.video = video
     }
 }
