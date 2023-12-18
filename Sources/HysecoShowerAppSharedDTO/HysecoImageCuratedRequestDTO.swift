2464f303e906317135d92b7076bd2597fcfa2ff0
Damien Theys <damien@hyseco.be>
2023-12-18 10:50:32 +0100

    update imagecurated

--- Sources/HysecoShowerAppSharedDTO/HysecoImageCuratedRequestDTO.swift
+++ Sources/HysecoShowerAppSharedDTO/HysecoImageCuratedRequestDTO.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+public struct HysecoImageCuratedRequestDTO: Codable {
+    public let title: String
+    public let legende: String
+    public let tri: Int
+    public let urlImage: String
+    
+    public init(title: String, legende: String, tri: Int, urlImage: String) {
+        self.title = title
+        self.legende = legende
+        self.tri = tri
+        self.urlImage = urlImage
+    }
+}
--- Sources/HysecoShowerAppSharedDTO/HysecoImageCuratedResponseDTO.swift
+++ Sources/HysecoShowerAppSharedDTO/HysecoImageCuratedResponseDTO.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public struct HysecoImageCuratedResponseDTO: Codable {
+    public let id: UUID
+    public let title: String
+    public let legende: String
+    public let tri: Int
+    public let urlImage: String
+    
+    public init(id: UUID, title: String, legende: String, tri: Int, urlImage: String) {
+        self.id = id
+        self.title = title
+        self.legende = legende
+        self.tri = tri
+        self.urlImage = urlImage
+    }
+}
