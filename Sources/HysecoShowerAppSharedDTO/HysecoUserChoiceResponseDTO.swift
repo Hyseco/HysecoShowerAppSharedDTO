6ef212633094d7e2754fb14c8a64cd2977de6919
Damien Theys <damien@hyseco.be>
2023-12-22 13:56:19 +0100

    ajout champ nom

--- Sources/HysecoShowerAppSharedDTO/HysecoUserChoiceResponseDTO.swift
+++ Sources/HysecoShowerAppSharedDTO/HysecoUserChoiceResponseDTO.swift
@@ -10,11 +10,12 @@ public struct HysecoUserChoiceResponseDTO: Codable {
     public let id: UUID
     public var optionCategoryId: UUID? = nil
     public var userChoiceCategoryId: UUID? = nil
+    public var userChoiceName: String
 
-
-    public init(id: UUID, userChoiceCategoryId: UUID, optionCategoryId: UUID) {
+    public init(id: UUID, userChoiceCategoryId: UUID, optionCategoryId: UUID, userChoiceName: String) {
         self.id = id
         self.userChoiceCategoryId = userChoiceCategoryId
         self.optionCategoryId = optionCategoryId
+        self.userChoiceName = userChoiceName
     }
 }
